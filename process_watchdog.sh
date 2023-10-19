d1beb436676100fa689fbf028b0355a011031905
ximatest <shizw2@163.com>
2023-10-19 21:18:56 +0800

    看门狗和启动脚本

--- process_watchdog.sh
+++ process_watchdog.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-process_dir=/home/test/install/bin
-process_list="5gc-udmd"
+process_dir=./install/bin
+process_list="5gc-amfd 5gc-amf-spsd 5gc-ausfd 5gc-bsfd 5gc-nrfd 5gc-nssfd 5gc-pcfd 5gc-smfd 5gc-udmd 5gc-udrd 5gc-upfd"
 log_file=/var/run/process_watchdog.log
 
 echo "`date`: $0 start!" > $log_file
-export LD_LIBRARY_PATH=/home/test/install/lib:$LD_LIBRARY_PATH
+export export LD_LIBRARY_PATH=./lib/x86_64-linux-gnu/:./lib64:$LD_LIBRARY_PATH
 ulimit -c unlimited
 
 while true;
@@ -13,7 +13,7 @@ do
 
     for process in $process_list
     do
-        process_no=`ps -ef | grep -i -E "/$process" | grep -v grep |grep -v log |grep -v watchdog | wc -l`
+        process_no=`ps -ef | grep -i -E "/$process" | grep -v grep |grep -v watchdog | wc -l`
         if [ ${process_no} -eq 0 ]; then
             echo "`date`: $process die, restart!" >> $log_file
             pkill $process > /dev/null
--- start5gc.sh
+++ start5gc.sh
@@ -1,4 +1,4 @@
-export export LD_LIBRARY_PATH=./lib/x86_64-linux-gnu/:$LD_LIBRARY_PATH
+export export LD_LIBRARY_PATH=./lib/x86_64-linux-gnu/:./lib64:$LD_LIBRARY_PATH
 pkill -9 5gc
 level="warn"
 spsno=1
@@ -15,18 +15,19 @@ if [  -n "$2" ] ;then
     spsno=$2
 fi
 
-./bin/5gc-nrfd -e $level &
-./bin/5gc-amfd -e $level &
-./bin/5gc-ausfd -e $level &
-./bin/5gc-udmd -e $level &
-./bin/5gc-pcfd -e $level &
-./bin/5gc-nssfd -e $level &
-./bin/5gc-bsfd -e $level &
-./bin/5gc-udrd -e $level &
-./bin/5gc-upfd -e $level &
+./install/bin/5gc-nrfd -e $level &
+./install/bin/5gc-amfd -e $level &
+./install/bin/5gc-ausfd -e $level &
+./install/bin/5gc-udmd -e $level &
+./install/bin/5gc-pcfd -e $level &
+./install/bin/5gc-nssfd -e $level &
+./install/bin/5gc-bsfd -e $level &
+./install/bin/5gc-udrd -e $level &
+./install/bin/5gc-upfd -e $level &
+./install/bin/5gc-smfd -e $level &
 
 for ((i=1; i<=$spsno; i++))
 do
-    ./bin/5gc-amf-spsd -i ${i} -e $level -l ./var/log/5gc/amf_sps${i}.log &
+    ./install/bin/5gc-amf-spsd -i ${i} -e $level -l ./install/var/log/5gc/amf_sps${i}.log &
 done
-./bin/smf/5gc-smfd -e $level &
+
