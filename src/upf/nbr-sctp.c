42ab635b1955550616576fcc435a6eb759000546
ximatest <ximatest@163.com>
2024-06-29 21:06:12 +0800

    d

--- src/upf/nbr-sctp.c
+++ src/upf/nbr-sctp.c
@@ -501,10 +501,10 @@ int nbr_open(void)
         filter_code,
     };
 
-    if(setsockopt(upf_self()->nbr_rawsocket, SOL_SOCKET, SO_ATTACH_FILTER, &filter_prog, sizeof(filter_prog)) == -1) {
+    /*if(setsockopt(upf_self()->nbr_rawsocket, SOL_SOCKET, SO_ATTACH_FILTER, &filter_prog, sizeof(filter_prog)) == -1) {
         perror("Failed to set socket filter");
         exit(EXIT_FAILURE);
-    }
+    }*/
 
     ogs_pollset_add(ogs_app()->pollset,
                 OGS_POLLIN, upf_self()->nbr_rawsocket, _gtpv1_nbr_recv_common_cb, upf_self()->nbr_rawsocket);
