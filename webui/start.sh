d2eda18db8cf224c461cf779977b1753f86b82a9
ximatest <shizw2@163.com>
2024-07-08 10:10:19 +0800

    d

--- webui/start.sh
+++ webui/start.sh
@@ -1,23 +1,41 @@
 #!/bin/bash
 
-# 杀死现有进程
-pkill node
+# 函数：检查服务是否存在，并使用systemctl启动
+start_service_if_exists() {
+    SERVICE_NAME="5gc-webui.service"
+    # 检查服务文件是否存在
+    if [ -e "/etc/systemd/system/$SERVICE_NAME" ]; then
+        echo "服务 $SERVICE_NAME 存在，使用 systemctl 启动服务。"
+        # 启动服务
+        systemctl start "$SERVICE_NAME"
+        # 等待服务启动
+        systemctl status "$SERVICE_NAME" --wait
+        exit 0
+    else
+        echo "服务 $SERVICE_NAME 不存在，按常规方式启动应用。"
+        # 常规启动代码
+        DB_URI=mongodb://127.0.0.1/5gc HOSTNAME=0.0.0.0 npm run start &
+        exit 0
+    fi
+}
 
-#如果脚本在别的目录下执行,必须要先进入webui目录，否则启动不成功
+# 杀死现有进程
+pkill -f 'node server'
+
+# 如果脚本在别的目录下执行, 必须要先进入webui目录，否则启动不成功
 cd /home/5gc/webui
 
 # 设置环境变量并启动服务
 if [ "$1" = "dev" ]; then
-    #dev版本每次启动都会自动构建
-    #注意,dev版本用于开发调试,不能后台运行(即使加了&)
+    # dev版本每次启动都会自动构建
     DB_URI=mongodb://127.0.0.1/5gc HOSTNAME=0.0.0.0 npm run dev &
 else
-    # rel版本需要先构建，再启动。 
-    # 检查传入的参数是否为 "nobuild",如果是,并且已经构建过，则不再构建(可提高启动速度)      
-    if [ "$1" == "nobuild" ] &&  [ -f ".next/BUILD_ID" ]; then
+    # rel版本需要先构建，再启动
+    if [ "$1" == "nobuild" ] && [ -f ".next/BUILD_ID" ]; then
         :
     else
-		npm run build
+        npm run build
     fi
-    DB_URI=mongodb://127.0.0.1/5gc HOSTNAME=0.0.0.0 npm run start &
+    # 检查并尝试启动服务
+    start_service_if_exists
 fi
